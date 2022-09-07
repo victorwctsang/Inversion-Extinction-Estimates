df6e051dc469710ef0682dffa26c6476258edfc5
Victor Tsang <victorwctsang@gmail.com>
2022-09-07 15:44:22 +1000

    Refactored functions and changed names

--- src/meme-functions.R
+++ src/meme-functions.R
@@ -3,30 +3,28 @@ library(extraDistr)
 estimate_CI.mc = function (
     alpha, n, K, W, u, eps.sigma, eps.mean, uniroot.interval
   ) {
-  CI.lower = getThetaQuantile(q=alpha/2, K, W, u, eps.mean, eps.sigma, uniroot.interval)
+  CI.lower = estimate_quantile.mc(q=alpha/2, K, W, u, eps.mean, eps.sigma, uniroot.interval)
   CI.upper = CI.lower
   if (alpha != 0.5) {
-    CI.upper = getThetaQuantile(q=1-alpha/2, K, W, u, eps.mean, eps.sigma, uniroot.interval)
+    CI.upper = estimate_quantile.mc(q=1-alpha/2, K, W, u, eps.mean, eps.sigma, uniroot.interval)
   }
   CI = list(CI.lower = CI.lower, CI.upper = CI.upper)
   return(CI)
 }
 
 estimate_quantile.mc = function (
-  q, K, W, u=NA, eps.mean=NA, eps.sigma=NA, uniroot.interval=NA
+  q, K, W, u=NA, eps.mean=0, eps.sigma=0, uniroot.interval=NA
 ) {
   n = length(W)
   m = min(W)
   theta_q.hat = NA
-  if (all(is.na(c(u, eps.mean, eps.sigma, uniroot.interval)))) {
+  if (eps.mean==0 & eps.sigma==0) {
     # No Measurement Error Case
     theta_q.hat = K - q^(-1/n) * (K-m)
-  } else if (any(is.na(c(u, eps.mean, eps.sigma, uniroot.interval)))) {
-    # Edge case
-    stop("Not all parameters for the measurement error estimation have been provided. Check your inputs and try again.")
-  } else {
+  } else  {
     # Measurement Error case
-    res = uniroot(function(theta) prod(getP(theta, K, u, m, eps.mean, eps.sigma)) - q)
+    res = uniroot(function(theta) prod(calc_P(theta, K, u, m, eps.mean, eps.sigma)) - q,
+                  interval=uniroot.interval)
     theta_q.hat = res$root
   }
   return(theta_q.hat)
@@ -36,9 +34,7 @@ calc_P = function (theta, K, u, m, eps.mean, eps.sigma) {
   # P(W >= m)
   F.eps.m = pnorm(m-theta, mean=eps.mean, sd=eps.sigma)
   F.eps.K = pnorm(K-theta, mean=eps.mean, sd=eps.sigma)
-  # Monte Carlo Integration to find phi.hat
   phi.hat = estimate_phi(u=u, mean=eps.mean, sd=eps.sigma, a=-Inf, b=m-theta, K=K, m=m, theta=theta)
-  F.eps.K = pnorm(K-theta, mean=eps.mean, sd=eps.sigma)
   P = 1 - F.eps.m/F.eps.K + phi.hat/F.eps.K
   return(P)
 }
