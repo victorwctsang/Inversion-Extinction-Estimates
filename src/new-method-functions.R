eab850415ef9ac331b1ca9987d47d4b5272d8f7d
Victor Tsang <victorwctsang@gmail.com>
2022-10-07 16:53:52 +1100

    changed phi to psi

--- src/new-method-functions.R
+++ src/new-method-functions.R
@@ -42,16 +42,16 @@ calc_P = function (theta, K, u, m, eps.mean, eps.sigma) {
   # P(W >= m)
   F.eps.m = pnorm(m-theta, mean=eps.mean, sd=eps.sigma)
   F.eps.K = pnorm(K-theta, mean=eps.mean, sd=eps.sigma)
-  phi.hat = estimate_phi(u=u, mean=eps.mean, sd=eps.sigma, a=-Inf, b=m-theta, K=K, m=m, theta=theta)
-  P = 1 - F.eps.m/F.eps.K * (1 + phi.hat)
+  psi.hat = estimate_psi(u=u, mean=eps.mean, sd=eps.sigma, a=-Inf, b=m-theta, K=K, m=m, theta=theta)
+  P = 1 - F.eps.m/F.eps.K * (1 - psi.hat)
   return(P)
 }
 
-estimate_phi = function (u, mean, sd, a, b, K, m, theta) {
+estimate_psi = function (u, mean, sd, a, b, K, m, theta) {
   # Monte Carlo integral
   e = uniform_to_tnorm(u, mean, sd, a, b)
-  phi.hat = apply((K-m)/(K-e-theta), 1, mean)
-  return(phi.hat)
+  psi.hat = apply((m-e-theta)/(K-e-theta), 1, mean)
+  return(psi.hat)
 }
 
 uniform_to_tnorm = function (u, mean, sd, a, b) {
