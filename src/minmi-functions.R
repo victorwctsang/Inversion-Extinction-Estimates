92b7e431789b5027fce3702cc07993d008cf3968
Victor Tsang <victorwctsang@gmail.com>
2022-10-23 11:40:18 +1100

    updated according to final delta method maths

--- src/minmi-functions.R
+++ src/minmi-functions.R
@@ -1,11 +1,10 @@
 library(extraDistr)
 
-estimate_extinction.minmi = function (W, sd, K, level = NULL, B = NULL, speed=F) {
+estimate_extinction.minmi = function (W, sd, K, level = NULL, B = NULL, time=F, .B_init=500) {
   result = list(extinction=NULL)
   n = length(W)
   m = min(W)
   dating.sd = mean(sd)
-  uniroot.interval = c(m-(K-m)/2, m+(K-m)/2)
   
   start_time = Sys.time()
   # Calculate number of monte carlo samples to use
@@ -13,51 +12,55 @@ estimate_extinction.minmi = function (W, sd, K, level = NULL, B = NULL, speed=F)
   if (!is.null(B)) {
     B.extinction = B.lower = B.upper = B
   } else {
+    u.init = runif(.B_init, 0, 1)
     B.extinction = find_optimal_B(max_var = (0.2*dating.sd)^2,
                                   q=0.5,
                                   K=K,
                                   m=m,
+                                  u=u.init,
                                   eps.mean=0,
-                                  eps.sigma=dating.sd,
-                                  B=500)
+                                  eps.sigma=dating.sd)
     if (!is.null(level)) {
       alpha = (1 - level)/2
       B.lower = find_optimal_B(max_var = (0.2*dating.sd)^2,
                                q=alpha,
                                K=K,
                                m=m,
+                               u=u.init,
                                eps.mean=0,
-                               eps.sigma=dating.sd,
-                               B=500)
+                               eps.sigma=dating.sd)
       B.upper = find_optimal_B(max_var = (0.2*dating.sd)^2,
                                q=1-alpha,
                                K=K,
                                m=m,
+                               u=u.init,
                                eps.mean=0,
-                               eps.sigma=dating.sd,
-                               B=500)
+                               eps.sigma=dating.sd)
     }
   }
   
   # Generate Monte Carlo Samples
   B.max = max(B.extinction, B.lower, B.upper)
-  u = runif(B.max, min=0, max=1)
+  mc.samples = runif(B.max, min=0, max=1)
 
   # Perform estimates  
   if (!is.null(level)) {
     alpha = (1 - level)/2
-    result$lower = estimate_quantile.minmi(q=alpha, K=K, W=W, u=u[1:B.lower],
+    result$lower = estimate_quantile.minmi(q=alpha, K=K, W=W, u=mc.samples[1:B.lower],
                                            eps.mean=0, eps.sigma=dating.sd,
                                            uniroot.interval=uniroot.interval)
-    result$upper = estimate_quantile.minmi(q=1-alpha, K=K, W=W, u=u[, 1:B.upper],
+    result$upper = estimate_quantile.minmi(q=1-alpha, K=K, W=W, u=mc.samples[, 1:B.upper],
                                            eps.mean=0, eps.sigma=dating.sd,
                                            uniroot.interval=uniroot.interval)
   }
-  result$extinction = estimate_quantile.minmi(q=0.5, K=K, W=W, u=u[1:B.extinction],
+  result$extinction = estimate_quantile.minmi(q=0.5, K=K, W=W, u=mc.samples[1:B.extinction],
                                               eps.mean=0, eps.sigma=dating.sd,
                                               uniroot.interval=uniroot.interval)
   
-  if (speed) {print(Sys.time() - start_time)}
+  if (time) {
+    result$time = Sys.time() - start_time
+    print(result$time)
+  }
   return(result)
 }
 
@@ -122,22 +125,23 @@ uniform_to_tnorm = function (u, mean, sd, a, b) {
   return(mc.samples)
 }
 
-find_optimal_B = function (max_var, q, K, m, eps.mean, eps.sigma, B=500) {
+find_optimal_B = function (max_var, q, K, m, u, eps.mean, eps.sigma) {
   # Initial estimate of theta_q using no measurement error case
   theta_q.hat.init = K - q^(-1/n)*(K-m) 
   
-  # Monte carlo samples
-  e = rtnorm(B, mean=eps.mean, sd=eps.sigma, a=-Inf, b=m-theta_q.hat.init)
-  
+  # pdfs and CDF evaluations (for convenience)
   f_eps.K = dnorm(K-theta_q.hat.init, eps.mean, eps.sigma)
   F_eps.K = pnorm(K-theta_q.hat.init, eps.mean, eps.sigma)
-  f_eps.m = dnorm(m-theta_q.hat.init, eps.mean, eps.sigma)
-  F_eps.m = pnorm(m-theta_q.hat.init, eps.mean, eps.sigma)
   
-  sigma2.psi_hat = var((m-e-theta_q.hat.init)/(K-e-theta_q.hat.init))
-  psi_hat = mean((m-e-theta_q.hat.init)/(K-e-theta_q.hat.init)) * F_eps.m
-  psi_hat.prime = mean((m-K)/(K-e-theta_q.hat.init)^2) * F_eps.m
-  u.prime = psi_hat * ( (F_eps.m * f_eps.K)/F_eps.K^2 - f_eps.m/F_eps.K) + F_eps.m/F_eps.K * psi_hat.prime
+  # Estimate sigma^2_var
+  B = length(u)
+  e = uniform_to_tnorm(u, eps.mean, eps.sigma, a=-Inf, b=m-theta_q.hat.init)
+  sample_var.psi_hat = var((m-e-theta_q.hat.init)/(K-e-theta_q.hat.init))
   
-  ceiling(sigma2.psi_hat/max_var * (F_eps.m/F_eps.K)^2 * (u.prime)^(-2))
+  # Estimate \hat\psi and \hat\psi prime
+  psi_hat = mean((m-e-theta_q.hat.init)/(K-e-theta_q.hat.init))
+  psi_hat.prime = - mean((K-m)/(K-e-theta_q.hat.init)^2)
+  
+  optimal_B = 1/max_var * sample_var.psi_hat * ( f_eps.K/F_eps.K * psi_hat + psi_hat.prime )^(-2)
+  return(optimal_B)
 }
