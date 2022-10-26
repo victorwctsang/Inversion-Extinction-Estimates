365ce7a5276c57170a419015bd14e384db571e0b
Victor Tsang <victorwctsang@gmail.com>
2022-10-26 15:34:44 +1100

    Broke simulation experiments into separate files

---
+++ src/simulate-data.R
@@ -0,0 +1,29 @@
+# Simulation Experiments
+# Generate synthetic datasets for simulation experiments
+
+library(tidyverse)
+source("helpers-simulation-experiments.R")
+
+options(nwarnings = 10000) 
+
+# Configure synthetic data generation
+
+synthetic.data.config = list(
+  seed = 2022,
+  theta.true = 10000,
+  K = 20000,
+  n.datasets = 100,
+  n = 20,
+  error_factors = c(0, 1, 2, 4),
+  dating_error.mean = 0,
+  dating_error.sd = 272
+)
+attach(synthetic.data.config)
+
+# Generate synthetic data
+set.seed(seed)
+datasets = simulate_datasets(n.datasets, error_factors, theta.true, K, dating_error.mean, dating_error.sd, n)
+
+
+# Export config and datasets
+save(synthetic.data.config, datasets, file = "../data/synthetic-data.RData")
