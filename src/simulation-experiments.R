274d2262867455f740df72ef27d7fec5f17732f6
Victor Tsang <victorwctsang@gmail.com>
2022-10-26 21:35:00 +1100

    Updated simulation experiments code

---
+++ src/simulation-experiments.R
@@ -0,0 +1,74 @@
+# Simulation Experiments
+
+# Load synthetic data and configuration
+source("helpers-simulation-experiments.R")
+
+load("../data/synthetic-data.RData")
+
+attach(synthetic.data.config)
+set.seed(seed)
+
+alpha = 0.05
+
+methods.point_estimates = c("MLE", "BA-MLE", "MINMI")
+methods.conf_int = c("MINMI")
+
+# Run trials
+
+results = data.frame(
+  id=integer(),
+  error_factor=double(),
+  q=double(),
+  method=character(),
+  estimate=double(),
+  time=double()
+)
+
+for (i in 1:nrow(datasets)) {
+  iter = datasets[i,]
+  W = iter$W[[1]]
+  print(paste("Dataset ID:", iter$id))
+  
+  # point estimates
+  for (method in methods.point_estimates) {
+    estimation = estimate_quantile(W=W, method=method, q=0.5, K=K, 
+                                   dating_error.mean=dating_error.mean,
+                                   dating_error.sd=iter$error_factor*dating_error.sd)
+    
+    results = tibble::add_row(results,
+                              id=iter$id,
+                              error_factor=iter$error_factor,
+                              q=estimation$q,
+                              method=method,
+                              estimate=estimation$estimate,
+                              time=estimation$runtime)
+  }
+  
+  # upper/lower estimates
+  for (method in methods.conf_int) {
+    estimation = estimate_quantile(W=W, method=method, q=alpha/2, K=K, 
+                                   dating_error.mean=dating_error.mean,
+                                   dating_error.sd=iter$error_factor*dating_error.sd)
+    results = tibble::add_row(results,
+                              id=iter$id,
+                              error_factor=iter$error_factor,
+                              q=estimation$q,
+                              method=method,
+                              estimate=estimation$estimate,
+                              time=estimation$runtime)
+    estimation = estimate_quantile(W=W, method=method, q=1-alpha/2, K=K, 
+                                   dating_error.mean=dating_error.mean,
+                                   dating_error.sd=iter$error_factor*dating_error.sd)
+    results = tibble::add_row(results,
+                              id=iter$id,
+                              error_factor=iter$error_factor,
+                              q=estimation$q,
+                              method=method,
+                              estimate=estimation$estimate,
+                              time=estimation$runtime)
+  }
+  
+}
+
+View(results)
+saveRDS(results, "simulation_experiment_results.RDS")
