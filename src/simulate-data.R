18dbb8d28b7992c36ce606bf5316797cc4c0b9e0
Victor Tsang <victorwctsang@gmail.com>
2022-10-26 21:32:20 +1100

    Increased number of sims

--- src/simulate-data.R
+++ src/simulate-data.R
@@ -12,9 +12,10 @@ synthetic.data.config = list(
   seed = 2022,
   theta.true = 10000,
   K = 20000,
-  n.datasets = 100,
+  n.datasets = 1000,
   n = 20,
   error_factors = c(0, 1, 2, 4),
+  n.error_factors = length(error_factors),
   dating_error.mean = 0,
   dating_error.sd = 272
 )
@@ -23,7 +24,6 @@ attach(synthetic.data.config)
 # Generate synthetic data
 set.seed(seed)
 datasets = simulate_datasets(n.datasets, error_factors, theta.true, K, dating_error.mean, dating_error.sd, n)
-
 
 # Export config and datasets
 save(synthetic.data.config, datasets, file = "../data/synthetic-data.RData")
