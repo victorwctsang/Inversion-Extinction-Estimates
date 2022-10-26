18043908162f551ba4487c77fc2171a50e4ec48b
Victor Tsang <victorwctsang@gmail.com>
2022-10-26 21:32:45 +1100

    implemented estimate_quantile wrapper function

--- src/helpers-simulation-experiments.R
+++ src/helpers-simulation-experiments.R
@@ -1,5 +1,6 @@
 
 library(readxl)
+source("minmi-functions.R")
 
 getFossilData = function (path, sheet, range, col_names, col_types) {
   read_excel(path, sheet, range, col_names, col_types)
@@ -42,10 +43,37 @@ simulate_datasets = function (n.sims, error_factor, theta.true, K, eps.mean, eps
   n.error_factors = length(error_factor)
   df.length = n.sims * n.error_factors
   
-  dataset.df = data.frame(sim_id=rep(1:n.sims, each=n.error_factors),
+  dataset.df = data.frame(id=1:df.length,
                           error_factor=rep(error_factor, length.out=df.length))
   W = lapply(dataset.df$error_factor, function(x) simulate_dataset(x, theta.true, K, eps.mean, eps.sigma, n))
   dataset.df$W = W
   return(dataset.df)
 }
 
+estimate_quantile = function (W, method, q, K, dating_error.mean, dating_error.sd) {
+  estimate=NA
+  runtime = NA
+  start_time=Sys.time()
+  estimate = switch(method,
+    MLE = if (q != 0.5) NULL else estimate_extinction.mle(W),
+    `BA-MLE` = if (q != 0.5) NULL else estimate_extinction.ba_mle(W, K),
+    STRAUSS = if (q != 0.5) NULL else estimate_extinction.strauss(W, K),
+    MINMI = estimate_quantile.minmi(q, K, W, u=runif(length(W), 0, 1), eps.mean=dating_error.mean, eps.sigma=dating_error.sd)
+  )
+  runtime = as.numeric(difftime(Sys.time(), start_time), units="secs")
+  return(list(q=q, estimate=estimate, runtime=runtime))
+}
+
+estimate_extinction.mle = function (W) {
+  return(min(W))
+}
+
+estimate_extinction.ba_mle = function (W, K) {
+  n = length(W)
+  return(min(W) * (n+1)/n - K/n)
+}
+
+estimate_extinction.strauss = function (W) {
+  n = length(W)
+  return((n*min(W) - max(W))/(n-1))
+}
