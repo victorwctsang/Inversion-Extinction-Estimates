14c60e4a7b929a7a1c44f0788d848f41d9d94ab2
Victor Tsang <victorwctsang@gmail.com>
2022-11-16 11:45:19 +1100

    Changed uniroot to use theta.test_vec and set default choice of theta.test_vec based on standard deviation approach.

--- src/simulated-inversion.R
+++ src/simulated-inversion.R
@@ -1,10 +1,18 @@
 
 library(mgcv)
 
-simulated_inversion = function (alpha, dates, sd, K, theta.test_vec, dating_error.mean=0, return_model=F) {
+simulated_inversion = function (alpha, dates, sd, K, theta.test_vec=NULL, dating_error.mean=0, return_model=F) {
   n.fossils = length(dates)
   n.theta.test_vec = length(theta.test_vec)
   dating_error.sd = mean(sd)
+  
+  flag.return_thetas = F
+  if (is.null(theta.test_vec)) {
+    theta.test_vec = seq(from = ba_mle(dates, K) - 10*sd(dates),
+                         to = min(dates) +2*sd(dates),
+                         length.out = 5000)
+    flag.return_thetas = T
+  }
   
   # Calculate S_star
   S_star = rep(NA, n.theta.test_vec)
@@ -27,8 +35,13 @@ simulated_inversion = function (alpha, dates, sd, K, theta.test_vec, dating_erro
   upper = get_min_est(theta.test_vec, model_gam, q=1-alpha/2)
   point = get_min_est(theta.test_vec, model_gam, q=0.5)
   results = list(lower=lower, upper=upper, point=point)
+  
+  # Return results
   if (return_model) {
     results$model = model_gam
+  }
+  if (flag.return_thetas) {
+    results$theta.test_vec = theta.test_vec
   }
   return(results)
 }
@@ -38,6 +51,11 @@ get_min_est <- function (theta.test_vec , model_gam, q){
     yval <- predict ( model_gam , newdata = data.frame ( theta.test_vec = theta.test_vec ), type = "response")
     return ( yval - q )
   }
-  theta_est <- uniroot ( prediction , lower = 5000 , upper = 15000, extendInt = "yes", model_gam = model_gam, maxiter = 2000)$root
+  theta_est <- uniroot ( prediction , lower = min(theta.test_vec) , upper = max(theta.test_vec), extendInt = "yes", model_gam = model_gam, maxiter = 2000)$root
   return ( theta_est )
+}
+
+ba_mle = function (X, K) {
+  n.X = length(X)
+  return(min(X) * (n.X + 1) / n.X - K / n.X)
 }
